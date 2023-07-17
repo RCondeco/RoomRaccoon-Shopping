98d50eeb0d855b20997fa7938f5f114e766f537a
RCondeco <106067665+RCondeco@users.noreply.github.com>
2023-07-17 17:11:27 +0100

    Update index.php

--- views/index.php
+++ views/index.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping List</title>
-    <link rel="stylesheet" href="../public/tailwind.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
-<body>
+<body body class="bg-gray-50 dark:bg-gray-900">
     <div class="container mx-auto p-4">
         <h1 class="text-2xl mb-4">Shopping List</h1>
         <form action="" method="post" class="mb-4">
